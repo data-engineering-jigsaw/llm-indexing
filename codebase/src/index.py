46e0e9d025a2c6b88b67154b074d9f53d6f04872
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-02-22 14:52:32 -0800

    update index

--- codebase/src/index.py
+++ codebase/src/index.py
@@ -1,40 +1,31 @@
 
 import os
 
-import fitz
-from llama_index.core import Document, VectorStoreIndex
+from llama_index.core import Document, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.indices.keyword_table import GPTKeywordTableIndex
+from llama_index.core.indices.tree import GPTTreeIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.openai import OpenAIEmbedding
 
-file_path = "../data/dev_report.pdf"
-doc = fitz.open(file_path)
+documents = SimpleDirectoryReader('data').load_data()
 
-doc_text = ""
-for doc_idx, page in enumerate(doc):
-    page_text = page.get_text("text")
-    doc_text += page_text
-
-
-documents = [Document(text = doc_text)]
-parser = SentenceSplitter(chunk_size=1024)
-nodes = parser.get_nodes_from_documents(documents)
-
-
+api_key = "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"
 os.environ['OPENAI_API_KEY'] =api_key
 embed_model = OpenAIEmbedding(api_key=api_key)
 
-index = VectorStoreIndex(nodes)
+# vector_store_index = VectorStoreIndex.from_documents(documents)
 
-query_engine = index.as_query_engine(response_mode="tree_summarize")
-response = query_engine.query("What is creative destruction according to the document?")
-response_text = response.response
+# keyword_index = GPTKeywordTableIndex.from_documents(documents)
 
-# And we can see where this came from with 
-response.source_nodes[0]
+# tree_index = GPTTreeIndex.from_documents(documents)
 
-# 
-# index.set_index_id("vector_index")
-# index.storage_context.persist("./storage")
-# storage_context = StorageContext.from_defaults(persist_dir="storage")
-# # load index
-# index = load_index_from_storage(storage_context, index_id="vector_index")
+
+# query_engine = index.as_query_engine()
+
+
+# query_engine = index.as_query_engine(response_mode="tree_summarize")
+# response = query_engine.query("What is creative destruction according to the document?")
+# response_text = response.response
+
+
+# response.source_nodes[0]
